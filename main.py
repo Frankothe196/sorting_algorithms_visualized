bd46c5344b4fdeee59af345aa33bc6b3bc0c9ced
frankothe196 <kipkalisbartoo@gmail.com>
2022-09-21 17:14:45 +0300

    changes

--- main.py
+++ main.py
@@ -1,88 +1,27 @@
 from flask import Flask, render_template ,request
 import numpy as np
-app = Flask(__name__)
-app.run(debug=True)
+import time
+from threading import Timer
+import asyncio
 
+app = Flask(__name__)
+data=''
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    global data
     if(request.method=="GET"):
-        return render_template("index.html",count=5, algorithm='algorithm',func=bucket_sort)
+        generate_arr(20)
+        return render_template("index.html", algorithm='algorithm', func=data)
     elif(request.method=="POST"):
-        if(request.form.get("algorithm")):
-            algorithm = request.form.get("algorithm")
-        else:
-            algorithm = 'empty'
-        
-        if(request.form.get("count")):
-            count = int(request.form.get("count"))
-        else:
-            count = 0
-        return render_template("index.html", count=count, algorithm=algorithm)
+        generate_arr(int(request.form.get('count')))
+        return render_template("index.html", algorithm='algorithm', func=data)
 
-
-def selection_sort():
-    #selection sorting here
-
-    #time complexity
-    time={}
-    time.best=""
-    time.average=""
-    time.worst=""
-    space=""
-
-def bubble_sort():
-    #bubble sorting here
-
-    #time complexity
-    time={}
-    time.best=""
-    time.average=""
-    time.worst=""
-    space=""
-    
-def insertion_sort():
-    #insertion sorting here
-
-    #time complexity
-    time={}
-    time.best=""
-    time.average=""
-    time.worst=""
-    space=""
-    
-def heap_sort():
-    #heap sorting here
-
-    #time complexity
-    time={}
-    time.best=""
-    time.average=""
-    time.worst=""
-    space=""
-    
-def quick_sort():
-    #Quick sorting here
-
-    #time complexity
-    time={}
-    time.best=""
-    time.average=""
-    time.worst=""
-    space=""
-    
-def bucket_sort():
-    #bucket sorting here
-
-    #time complexity
-    # time={}
-    # time.best=""
-    # time.average=""
-    # time.worst=""
-    # space=""
-    randArr = np.random.randint(20,size=20)
+def generate_arr(len):
+    global data
+    randArr = np.random.randint(20,size=len)
     max=randArr.max()
-    print(randArr)
-    print(randArr.tolist())
-    lst = [x for x in range(20)]
-    return [randArr.tolist(), max]
-    
+    data= [randArr.tolist(), max]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
