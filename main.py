10fe3e120becb7382eb8ad09bf1a4f2d2aed7ea3
frankothe196 <kipkalisbartoo@gmail.com>
2022-11-02 14:03:43 +0300

    merge sort complete

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template ,request
 import numpy as np
-import time
-from threading import Timer
-import asyncio
+#import time
+#from threading import Timer
+#import asyncio
 
 app = Flask(__name__)
-data=''
+
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    global data
     if(request.method=="GET"):
-        generate_arr(20)
+        data = generate_arr(20)
         return render_template("index.html", algorithm='algorithm', func=data, count=20, type='selection')
     elif(request.method=="POST"):
         type = request.form.get('algorithm')
         count = int(request.form.get('count'))
-        generate_arr(count)
+        data = generate_arr(count)
         return render_template("index.html", algorithm='algorithm', func=data, type=type, count=count)
 
 def generate_arr(len):
-    global data
     randArr = np.random.randint(20,size=len)
     max=randArr.max()
     data= [randArr.tolist(), max]
+    return data
 
 
 if __name__ == '__main__':
