245aeafe5d97ab3f7f56d216b5767c8c43efd1f7
frankothe196 <kipkalisbartoo@gmail.com>
2022-11-04 09:13:20 +0300

    Legend populated, app V1 complete

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main():
     if(request.method=="GET"):
-        data = generate_arr(20)
-        return render_template("index.html", algorithm='algorithm', func=data, count=20, type='selection')
+        arrLength=30
+        data = generate_arr(arrLength)
+        legend=[['complete','goldenrod'],['iterator','green'],['swapped','red']]
+        return render_template("index.html", algorithm='algorithm', func=data, type='selection', count=arrLength, legend=legend)
     elif(request.method=="POST"):
         type = request.form.get('algorithm')
         count = int(request.form.get('count'))
         data = generate_arr(count)
         legend=''
         if(type=='selection'):
-            legend=[['complete','#fff'],['current','#000'],['complete','#fff']]
+            legend=[['complete','goldenrod'],['iterator','green'],['swapped','red']]
         elif(type=='bubble'):
-            legend=[['complete','#fff'],['current','#000'],['complete','#fff']]
+            legend=[['complete','goldenrod'],['iterator','green'],['spapped','red']]
         elif(type=='merge'):
-            legend=[['complete','#fff'],['current','#000'],['complete','#fff']]
+            legend=[['complete','goldenrod'],['Iterator','green']]
         elif(type=='quick'):
-            legend=[['complete','#fff'],['current','#000'],['complete','#fff']]
+            legend=[['complete','goldenrod'],['Pivot','yellow'],['Swap or iterator','red'],['inactive','white']]
 
         return render_template("index.html", algorithm='algorithm', func=data, type=type, count=count, legend=legend)
 
