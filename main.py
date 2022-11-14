5c31426c8f38c89758bc5ec18a13c939802f0cf6
frankothe196 <kipkalisbartoo@gmail.com>
2022-11-14 12:22:11 +0300

    Repository complete

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template ,request
 import numpy as np
-#import time
-#from threading import Timer
-#import asyncio
 
 app = Flask(__name__)
 
+# There will only be one available route for this app as it is a single page app with a simple APIs
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    # Display the defauly starting screen
     if(request.method=="GET"):
         arrLength=30
         data = generate_arr(arrLength)
         legend=[['complete','goldenrod'],['iterator','green'],['swapped','red']]
         return render_template("index.html", algorithm='algorithm', func=data, type='selection', count=arrLength, legend=legend)
+    # When a user interacts with the input form
     elif(request.method=="POST"):
         type = request.form.get('algorithm')
         count = int(request.form.get('count'))
         data = generate_arr(count)
         legend=''
+        # Setting the legenf for the selected algorithm
         if(type=='selection'):
             legend=[['complete','goldenrod'],['iterator','green'],['swapped','red']]
         elif(type=='bubble'):
@@ -27,10 +28,12 @@ def main():
         elif(type=='quick'):
             legend=[['complete','goldenrod'],['Pivot','yellow'],['Swap or iterator','red'],['inactive','white']]
 
-        return render_template("index.html", algorithm='algorithm', func=data, type=type, count=count, legend=legend)
+        # Data shared in the response is the 1. A random array generated for sorting. 2. The number of items in the array. 2. The type of algorithm to run.  3. The legend for the selected algorithm.
+        return render_template("index.html",  func=data, count=count, type=type, legend=legend)
 
+# Generate a random array, length or array is specified in function parameter
 def generate_arr(len):
-    randArr = np.random.randint(20,size=len)
+    randArr = np.random.randint(30,size=len)
     max=randArr.max()
     data= [randArr.tolist(), max]
     return data
